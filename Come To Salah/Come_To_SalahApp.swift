13b5c7433772d147d4d183d91bee5fb8b8eb80fe
MahinMuhammad <amimahinurrahman@gmail.com>
2024-02-17 17:23:58 +0600

    added .gitignore

---
+++ Come To Salah/Come_To_SalahApp.swift
@@ -0,0 +1,17 @@
+//
+//  Come_To_SalahApp.swift
+//  Come To Salah
+//
+//  Created by Md. Mahinur Rahman on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Come_To_SalahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
